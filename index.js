aa2eba40184dd20ecddd147616d717d89fb20fc9
LAZYSHOUTDESK\user <tim70036@gmail.com>
2018-09-13 00:07:59 +0800

    facebbok authentication init

--- index.js
+++ index.js
@@ -1,27 +1,80 @@
 // Load the things we need
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+var credentials = require('./credentials');
+
+/////////////////////////////
+// Only for test
+var fs = require('fs');
+var https = require('https');
+var privateKey  = fs.readFileSync('./ssl/ico.pem', 'utf8');
+var certificate = fs.readFileSync('./ssl/ico.crt', 'utf8');
+var ssl = { key: privateKey, cert: certificate };
+/////////////////////////////
+
 var app = express();
 
-//app.enable('view cache');
+
+
 
 // Set the view engine
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// Set static middleware 
-app.use(express.static(__dirname + '/public'));
+//app.enable('view cache');
 
-// Routing
+// Set middleware 
+app.use(express.static(__dirname + '/public')); // for static content
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Set authentication 
+var auth = require('./lib/auth.js')(app, {
+    providers: credentials.authProviders, 
+    successRedirect: '/', 
+    failureRedirect: '/',
+});
+
+// Links in Passport middleware
+auth.init();
+
+// Set authentication routes
+auth.registerRoutes();
+
+
+// Set render page routes
 // index page 
 app.get('/', function(req, res) {
     res.render('index',  {layout: false} ); // not using layout
 });
-
 // login page
 app.get('/login', function(req, res) {
-    res.render('index',  {layout: false} ); // not using layout
+    res.render('login-light',  {layout: false} ); // not using layout
+});
+// signup page
+app.get('/signup', function(req, res) {
+    res.render('signup-light',  {layout: false} ); // not using layout
 });
 
-app.listen(8080);
-console.log('8080 is the magic port');
+// Set form handling routes
+app.post('/signup', function(req, res) {
+    console.log("receive shit");
+
+    // Return to home page
+    res.redirect(303, '/')
+});
+
+
+
+// app.listen(8080);
+// console.log('8080 is the magic port');
+
+
+/////////////////////////////
+// Only for test
+https.createServer(ssl, app).listen(8080);
+console.log('Using https at 8080 port for development use...');
+/////////////////////////////
